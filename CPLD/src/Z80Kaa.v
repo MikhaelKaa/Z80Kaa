c104f49f95aa7a6639f8fa0434bd1b91b7aba54e
MichailKaa <unionfalse@gmail.com>
2023-11-27 00:31:02 +0300

    >> Переделал работу с LCD1602.

--- CPLD/src/Z80Kaa.v
+++ CPLD/src/Z80Kaa.v
@@ -17,7 +17,6 @@ input rst,
 // enable
 input busrq,
 
-
 // led
 output led,
 
@@ -27,46 +26,77 @@ output lcd_rw,
 output lcd_rs,
 
 // Keyboard
-output KBD
+output KBD,
 
+// div
+output div
 );
 
-reg [4:0] clk_div;
+reg [3:0] clk_div = 4'b0;
+reg [3:0] clk_cnt = 4'b0;
 always@(negedge in_clock) begin
 	clk_div = clk_div + 1'b1;
-	
+	if (reg_ff == clk_cnt) begin
+		pre_div = ~pre_div;
+		clk_cnt = 0;
+	end
+	else clk_cnt = clk_cnt + 1'b1;
 end
 
 // Z80 clock.
 assign cpu_clock = clk_div[1];
-//assign cpu_clock = reg_fe[3]?clk_div[4]:clk_div[3];
+//assign cpu_clock = clk_div[reg_ff];
+//assign cpu_clock = reg_ff[0]?clk_div[3]:clk_div[2];
+
+reg pre_div = 1'b0;
+assign div = pre_div;
+// io
+wire iowr = iorq | wr;
+wire iord = iorq | rd;
+
+//   port z80                        read        write
+wire port_0xf8 = (adr == 3'b000); // 
+wire port_0xf9 = (adr == 3'b001); // 
+wire port_0xfa = (adr == 3'b010); // 
+wire port_0xfb = (adr == 3'b011); // 
+wire port_0xfc = (adr == 3'b100); // lcd cmd 	 lcd cmd
+wire port_0xfd = (adr == 3'b101); // lcd data	 lcd data
+wire port_0xfe = (adr == 3'b110); // keyboard    reg_fe
+wire port_0xff = (adr == 3'b111); //				 div [3:0]
+
+wire port_lcd = adr[2] & ~adr[1];
+
+wire kbd_rd = (iord | ~(port_0xfe));
+assign KBD    = busrq?((kbd_rd)?(1'bz):(1'b0)):(1'bz); // fe
+
+assign lcd_e  = busrq?(~(iowr) & port_lcd):(1'b0);  // fd
+assign lcd_rw = busrq?(1'b0):(1'b0); // TODO Write only
+assign lcd_rs = busrq?(adr[0]):(1'b1);
+
+// register fe
+reg [7:0] reg_fe = 8'b0;
+// register ff
+reg [3:0] reg_ff = 4'b0;
+
+
+always @(negedge iowr or negedge rst) begin
+	if(rst == 1'b0) begin
+		reg_fe = 8'b0;
+		reg_ff = 4'b0011;
+	end else
+	if(port_0xfe) begin
+		reg_fe = data;
+	end else
+	if(port_0xff) begin
+		reg_ff = data[3:0];
+	end
+end
+
+assign led    = /*busrq?(*/reg_fe[0]/*):(1'b1)*/;
 
 // M48Z35Y
 //assign E = mreq;
 //assign G = rd;
 //assign W = wr;
 
-
-// io
-wire IOWR = iorq | wr;
-wire IORD = iorq | rd;
-wire port_fe = (adr == 3'b110);
-wire port_fd = (adr == 3'b101);
-// testpin
-reg [7:0] reg_fe = 8'b1;
-
-always @(negedge IOWR or negedge rst) begin
-	if(rst == 1'b0) begin
-		reg_fe = 8'b0;
-	end else
-	if(port_fe) begin
-		reg_fe = data;
-	end
-end
-
-assign led =    busrq?(reg_fe[0]):(1'b1);
-assign lcd_e =  busrq?(~(IOWR | ~(port_fd))):(1'b1);  //fd
-assign lcd_rw = busrq?(reg_fe[1]):(1'b1);
-assign lcd_rs = busrq?(reg_fe[2]):(1'b1);
-assign KBD =    busrq?((IORD | ~(port_fe))?(1'bz):(1'b0)):(1'bz); //fe
 endmodule
--- FW/hello.asm
+++ FW/hello.asm
@@ -15,7 +15,25 @@ interrupt
     push af
     push bc
     ;int programm
+    ld a, 0b00000000
+    out (0xfe), a
+    ld bc, 1000
+    call delay_bc
 
+    ld a, 0b00000001
+    out (0xfe), a
+    ld bc, 1000
+    call delay_bc
+
+    ld a, 0b00000000
+    out (0xfe), a
+    ld bc, 1000
+    call delay_bc
+
+    ld a, 0b00000001
+    out (0xfe), a
+    ld bc, 1000
+    call delay_bc
     ; end int programm
     ld bc, 256
 intdelay
@@ -35,6 +53,28 @@ start:
     ; Разрешаем прерывания.
     ei    
     ld a,0
+
+    ld a, 0b00000000
+    out (0xfe), a
+    ld bc, 4000
+    call delay_bc
+
+    ld a, 0b00000001
+    out (0xfe), a
+    ld bc, 4000
+    call delay_bc
+
+    ld a, 0b00000000
+    out (0xfe), a
+    ld bc, 4000
+    call delay_bc
+
+    ld a, 0b00000001
+    out (0xfe), a
+    ld bc, 4000
+    call delay_bc
+
+    ;call LCD1602_init
 loop:
     ; Увеличиваем регистр а.
     ;inc a
@@ -57,99 +97,74 @@ loop:
     jp (skip)
 
 up
-    call print1602  ;LCD1602
+    ld hl, Text
+    call print1602_hl  ;LCD1602
     jp (skip)
 
 down
-    call LCD1602_init
+    call lcd_init;LCD1602_init
     jp (skip)
 ok
-    ld a, 0b00000000
-    out (0xfe), A
-    call delay100
-    call delay100
-    call delay100
-    call delay100
-    
-    ld a, 0b00000001
-    out (0xfe), A
+    ld a, (cnt)
+    dec a
+    ld (cnt), a
+    out (0xff), a
+    ld bc, 4000
+    call delay_bc
 skip
     ; Задержка.
-    call delay100
+    ld bc, 25000
+    call delay_bc
 
     jr loop 
 
 ; Процедура инициализвции LCD1602
-LCD1602_init
-    push af
-    push bc
-    ld a, 0x38
-    call LCD1602_CMD
-    ld a, 0x0c
-    call LCD1602_CMD
-    ld a, 0x06
-    call LCD1602_CMD
-    ld a, 0x01
-    call LCD1602_CMD
-    ld a, 0x80
-    call LCD1602_CMD
-    pop bc
-    pop af
-    ret
-
-
-print1602
-    ld hl, Text
-prloop
+lcd_init
+    ld hl, lcd_init_seq
+    ld bc, 5
+lcd_init_loop
     ld a,(hl)
-    and a
-    ret z
-    call LCD1602_DATA
-    inc hl
-    jr prloop
-
-Text db "Z80Kaa board", 0
-
-
-LCD1602_CMD
-    call delay100
-    push af
-    ld a, 0b10000000
-    out (0xfe), a
-    call delay100
-    pop af
-    out (0xfd), a
-    call delay100
-    ret
-
-LCD1602_DATA
-    call delay100
-    push af
-    ld a, 0b10000100
-    out (0xfe), a
-    call delay100
-    pop af
-    out (0xfd), a
-    call delay100
-    ret
-
-; Процедура задержки
-delay100
-    push af
+    out (0xfc), a
     push bc
-    ld bc, 100
-delay
+    ld bc, 300
+    call delay_bc
+    inc hl
+    pop bc
     dec bc
     ld a, b
     or c
-    jr nz, delay
-    pop bc
+    jr nz, lcd_init_loop
+    ret
+
+lcd_init_seq db 0x38, 0x0c, 0x06, 0x01, 0x80
+
+print1602_hl
+    ld a,(hl)
+    and a
+    ret z
+    out (0xfd), a
+    ld bc, 300
+    call delay_bc
+    inc hl
+    jr print1602_hl
+
+Text db "Z80Kaa board ", 0
+
+; Процедура задержки
+delay_bc
+    push af
+    ;push bc
+delay_loop
+    dec bc
+    ld a, b
+    or c
+    jr nz, delay_loop
+    ;pop bc
     pop af
     ret
 
 cnt db 0
-portfe db 0
-
+;portfe db 0
 
 end:
     ; Выводим размер банарника.
